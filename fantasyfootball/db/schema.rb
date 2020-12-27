b60b144825a9c2113a74ec246e6e135b7a2e9787
john million <jpmillion@comcast.net>
2020-12-27 10:02:01 -0500

    created table leagues

--- fantasyfootball/db/schema.rb
+++ fantasyfootball/db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201227145611) do
+ActiveRecord::Schema.define(version: 20201227145956) do
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "players", force: :cascade do |t|
     t.string  "name"
