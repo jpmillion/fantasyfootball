36e9f7d6b81e80040d3103fcf4d569be13d7977e
john million <jpmillion@comcast.net>
2020-12-27 10:08:54 -0500

    modified user model to have many leagues through users_leagues and teams through leagues

--- fantasyfootball/app/models/user.rb
+++ fantasyfootball/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
     has_secure_password
-    has_many :teams
+    has_many :leagues, through: :users_leagues
+    has_many :teams, through: :leagues
     has_many :players, through: :teams
 
 end
