b6bd969e535f0ec2dd19a99677a167eb6a6fd290
john million <jpmillion@comcast.net>
2020-12-27 11:35:01 -0500

    created index route, users/index route, user/new route

---
+++ fantasyfootball/app/controllers/user_controller.rb
@@ -0,0 +1,12 @@
+class UserController < ApplicationController
+
+    get '/users' do
+        @users = User.all 
+        erb :'users/index'
+    end
+
+    get '/users/new' do
+        erb :'users/new'
+    end
+
+end
