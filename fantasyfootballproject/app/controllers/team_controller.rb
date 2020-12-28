7a5a444e60f08e04f41fae9da9fb46999103db21
john million <jpmillion@comcast.net>
2020-12-27 19:31:10 -0500

    create action for teams

---
+++ fantasyfootballproject/app/controllers/team_controller.rb
@@ -0,0 +1,18 @@
+class TeamController < ApplicationController
+
+    get '/teams/new' do
+        redirect '/login' if !logged_in?
+        @leagues = League.all
+        erb :'teams/new' 
+    end
+
+    post '/teams' do
+        league = League.find(params[:league_id])
+        redirect '/teams/new' if params[:team_name] == '' || !league || league.teams.detect {|team| team.name == params[:team_name]}
+        team = Team.new(name: params[:team_name])
+        team.user = current_user
+        team.league = league
+        team.save
+        redirect "/users/#{current_user.id}"
+    end
+end
--- fantasyfootballproject/config.ru
+++ fantasyfootballproject/config.ru
@@ -5,7 +5,7 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use Rack::MethodOverride
-
+use TeamController
 use LeagueController
 use SessionController
 use UserController
