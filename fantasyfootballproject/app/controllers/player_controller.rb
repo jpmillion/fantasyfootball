40cd0280418260128f2dccafa6419c3be88b4ecd
john million <jpmillion@comcast.net>
2021-01-05 01:34:55 -0500

    refractored players controller

--- fantasyfootballproject/app/controllers/player_controller.rb
+++ fantasyfootballproject/app/controllers/player_controller.rb
@@ -7,8 +7,8 @@ class PlayerController < ApplicationController
     post '/players' do
         team = Team.find(params[:team_id]) if params[:team_id] != nil
         redirect "/teams/#{team.id}" if params[:name] == '' || params[:position] == nil || team.league.players.any? {|player| player.name == params[:name]}
-        player = Player.new(name: params[:name], position: params[:position])
-        team.players << player
+        
+        team.players << Player.new(name: params[:name], position: params[:position])
         redirect "/teams/#{team.id}"
     end
 
